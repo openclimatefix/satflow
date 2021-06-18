e534cf29e4a58f08dec96eecc9a7ed41c306e7ab
Jacob Bieker <jacob@openclimatefix.org>
2021-06-18 21:34:16 +0100

    Add iterable dataset
    Should use a lot less RAM, as loading att ~144 sets of images grows to more than 30GB of ram at once. Is pushing me to more go towards Zarr probably, but one last try for webdataset for this thing. A lot more limitations with this one compared to the other SatFlowDataset, but tradeoffs it for memory

--- satflow/data/datasets.py
+++ satflow/data/datasets.py
@@ -13,6 +13,9 @@ import pickle
 import numpy.lib.format
 import io
 
+import braceexpand
+
+
 from typing import Dict, Any, Type, List, Sequence
 
 REGISTERED_DATASET_CLASSES = {}
@@ -298,6 +301,168 @@ class SatFlowDataset(thd.IterableDataset, wds.Shorthands, wds.Composable):
                                 "image"
                             ]
                             target_mask = self.aug.replay(replay, image=target_mask)[
+                                "image"
+                            ]
+                            # Convert to Channel x Time x W x H
+                            image = np.moveaxis(image, [3], [1])
+                            target_image = np.moveaxis(target_image, [2], [0])
+                            target_mask = np.expand_dims(target_mask, axis=0)
+
+                            if self.use_time and self.time_aux:
+                                time_layer = create_time_layer(target_timestep, self.output_shape)
+                                yield image, time_layer, target_image, target_mask
+                            yield image, target_image, target_mask
+
+
+@register_dataset
+class SatFlowIterDataset(SatFlowDataset):
+    """
+    SatFlow dataset that works with WebDataset format where each tar file is 1 day, and each frame is a sample, iterated
+    in order. So after getting a sample, iterate through it all, building up the samples from earliest to latest
+    """
+
+    def __init__(self, datasets, config, train=True):
+        urls = list(braceexpand.braceexpand(datasets))
+        datasets = [wds.WebDataset(src) for src in urls] # This is to ensure that each day is kept together
+        super(SatFlowIterDataset, self).__init__(datasets, config, train)
+        self.topo = np.load("../resources/cutdown_europe_dem.npy")
+        self.topo = self.topo - np.min(self.topo) / (np.max(self.topo) - np.min(self.topo))
+        self.topo = np.expand_dims(self.topo, axis=-1)
+        self.location = np.load("../resources/location_array.npy")
+        # Need to create topo and location arrays here fpr use later
+
+    def get_timestep(self, sample, idx, return_target=False):
+        """
+        Gets the image stack of the given timestep, if return_target is true, only returns teh mask and satellite channels
+        as the model does not need to predict the time, topogrpahic data, etc.
+        :param sample:
+        :param idx:
+        :param return_target:
+        :return:
+        """
+        image = np.stack(
+            [load_np(sample[f"{b.lower()}.npy"]) for b in self.bands], axis=-1
+        )
+
+        # Regularize here
+        image = (image - MSG_MIN) / (MSG_MAX - MSG_MIN)
+
+        target = load_np(sample[f"{self.target_type}.npy"])
+        if "mask" in self.target_type:
+            target = binarize_mask(
+                target
+            )  # Not actual target, but for now, should be good
+
+        if return_target:
+            return image, target
+
+        if self.use_topo:
+            image = np.concatenate([image, self.topo], axis=-1)
+        if self.use_latlon:
+            image = np.concatenate([image, self.location], axis=-1)
+        if self.use_time and not self.time_aux:
+            t = create_time_layer(
+                pickle.loads(sample["time.pyd"]),
+                shape=(image.shape[0], image.shape[1]),
+            )
+            image = np.concatenate([image, t,], axis=-1,)
+        if self.use_mask:
+            image = np.concatenate(
+                [
+                    image,
+                    np.expand_dims(
+                        binarize_mask(load_np(sample[f"cloudmask.npy"])),
+                        axis=-1,
+                    ),
+                ],
+                axis=-1,
+            )
+
+        return image, target
+
+    def __iter__(self) -> Iterator[T_co]:
+        # Need to make sure same time step for all of them.
+        # As its all from rapid scan, should be fairly easy.
+        # Main missing one is the regional and rapid weather ones, which are every 15 minutes,
+        # but could be interpolated between the previous step and next one by weighting by time difference
+        # Topographic is same of course, just need to resize to 1km x 1km?
+        # grid by taking the mean value of the interior ones
+        sources = [iter(ds) for ds in self.datasets]
+        while True:
+            np.random.shuffle(sources) # Shuffle order of the days
+            for source in sources:
+                # Check to make sure all timesteps exist
+                # Times that have enough previous timesteps and post timesteps for training
+                # pick one at random
+                # TODO Get better way of doing this rather than hardcoding 144, but for now, lets do it
+                idxs = np.random.randint(
+                    self.num_timesteps * self.skip_timesteps,
+                    144 - self.forecast_times,
+                    size=1, # TODO Change back to num crops when better
+                    )
+                # Need to get lowest to largest to keep iteration going
+                idxs = np.sort(idxs)
+                for idx in idxs:
+                    # Get current and prev images
+                    target_timesteps = (
+                            np.random.randint(
+                                idx + 1, idx + self.forecast_times, size=self.num_crops
+                            )
+                            - idx
+                    )
+                    target_timesteps = np.sort(target_timesteps)
+                    s_timesteps = target_timesteps + idx
+                    first_sample_idx = idx - (self.num_timesteps * self.skip_timesteps)
+                    future_targets = []
+                    future_images = []
+                    curr_images = []
+                    for sample_idx, sample in source: # Go through one day
+                        if sample_idx == first_sample_idx: # First one
+                            image, _ = self.get_timestep(
+                                sample, idx - self.num_timesteps
+                            )  # First timestep considered
+                            image = np.expand_dims(image, axis=0)
+                            curr_images.append(image)
+                        elif sample_idx in np.arange(idx - ((self.num_timesteps - 1) * self.skip_timesteps), idx+self.skip_timesteps, self.skip_timesteps):
+                            t_image, _ = self.get_timestep(sample, idx)
+                            curr_images.append(t_image)
+                        elif sample_idx in s_timesteps: # In the future ones
+                            target_image, target_mask = self.get_timestep(
+                                sample, sample_idx, return_target=True
+                            )
+                            future_images.append(target_image)
+                            future_targets.append(target_mask)
+                    # Now do the post/pre processing
+                    # First check if enough samples exist
+                    if len(curr_images) != (self.num_timesteps + 1) or len(future_images) != len(target_timesteps):
+                        print(f"Curr Wanted: {(self.num_timesteps + 1)} Curr Got: {len(curr_images)} "
+                              f"Future Wanted: {len(target_timesteps)} Future Got: {len(future_images)}")
+                        continue
+
+                    # Now do the cropping, but keep the original, adding the timecube, etc.
+                    for i, target_timestep in enumerate(s_timesteps):
+                        time_cube = self.create_target_time_cube(target_timestep)
+                        for _ in self.num_crops:
+                            data = self.aug(image=curr_images[0])
+                            replay = data["replay"]
+                            image = data["image"]
+                            if self.use_time and not self.time_aux:
+                                image = np.concatenate([image, time_cube], axis=-1)
+                            image = np.expand_dims(image, axis=0)
+                            for t_image in curr_images[1:]:
+                                t_image = self.aug.replay(replay, image=t_image)[
+                                    "image"
+                                ]
+                                if self.use_time and not self.time_aux:
+                                    t_image = np.concatenate([t_image, time_cube], axis=-1)
+                                image = np.concatenate(
+                                    [image, np.expand_dims(t_image, axis=0)]
+                                )
+                            # Now in a Time x W x H x Channel order
+                            target_image = self.aug.replay(replay, image=future_images[i])[
+                                "image"
+                            ]
+                            target_mask = self.aug.replay(replay, image=future_targets[i])[
                                 "image"
                             ]
                             # Convert to Channel x Time x W x H
