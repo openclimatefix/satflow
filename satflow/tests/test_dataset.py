5b3e53af9668745b008d9ce8f464e618f7969655
Jacob Bieker <jacob@openclimatefix.org>
2021-07-06 11:11:52 +0100

    Start on tests

--- satflow/tests/test_dataset.py
+++ satflow/tests/test_dataset.py
@@ -1,12 +1,25 @@
 # NIR1.6, VIS0.8 and VIS0.6 RGB for near normal view
 import numpy as np
-import matplotlib.pyplot as plt
 from satflow.data.datasets import SatFlowDataset, CloudFlowDataset
 import webdataset as wds
+import yaml
+
+
+def load_config(config_file):
+    with open(config_file, "r") as cfg:
+        return yaml.load(cfg, Loader=yaml.FullLoader)["config"]
 
 
 def test_satflow():
-    dataset = wds.WebDataset("../../datasets/satflow-flow-4-tiled-00001.tar").decode()
-    sources = iter(dataset)
+    dataset = wds.WebDataset("../../datasets/satflow-multi-test.tar").decode()
+    pass
 
+
+def test_cloudflow():
+    dataset = wds.WebDataset("../../datasets/satflow-test.tar").decode()
+    # d = next(iter(dataset))
+    # print(d["time.pyd"])
+    config = load_config("configs/satflow.yaml")
+    cloudflow = CloudFlowDataset(dataset, config)
+    data = next(iter(cloudflow))
     pass
