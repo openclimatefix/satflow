948e7ae32f3836666b8071cfd7c103047678be74
Jacob Bieker <jacob@openclimatefix.org>
2021-07-08 17:14:09 +0100

    Optical Flow somewhat works
    Have to use satellite image to get optical flow, cloud mask doesn't do a good job of it, still seems to not work super well? But we'll see, still need to add metrics

--- satflow/baseline/optical_flow.py
+++ satflow/baseline/optical_flow.py
@@ -18,17 +18,55 @@ dset = wds.WebDataset("/run/media/jacob/data/satflow-flow-144-tiled-{00001..0014
 
 dataset = OpticalFlowDataset([dset], config=config)
 
+import matplotlib.pyplot as plt
+
+
+def warp_flow(img, flow):
+    h, w = flow.shape[:2]
+    flow = -flow
+    flow[:, :, 0] += np.arange(w)
+    flow[:, :, 1] += np.arange(h)[:, np.newaxis]
+    res = cv2.remap(img, flow, None, cv2.INTER_LINEAR)
+    return res
+
+
 for data in dataset:
-    prev_frame, curr_frame, next_frames = data
+    prev_frame, curr_frame, next_frames, image, prev_image = data
     prev_frame = np.moveaxis(prev_frame, [0], [2])
     curr_frame = np.moveaxis(curr_frame, [0], [2])
-    print(prev_frame.shape)
-    print(curr_frame.shape)
-    print(next_frames.shape)
-    flow = cv2.calcOpticalFlowFarneback(prev_frame, curr_frame, None, 0.5, 3, 15, 3, 5, 1.2, 0)
-    print(flow.shape)
+    flow = cv2.calcOpticalFlowFarneback(prev_image, image, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+    plt.imshow(prev_frame)
+    plt.imshow(image)
+    plt.title("Image")
+    plt.show()
+    plt.imshow(prev_image)
+    plt.title("Prev Image")
+    plt.show()
+    plt.imshow(image - prev_image)
+    plt.title("Image Diff")
+    plt.show()
+    plt.imshow(curr_frame - prev_frame)
+    plt.title("Difference")
+    plt.show()
+    warped_frame = warp_flow(curr_frame.astype(np.float32), flow)
+    warped_frame = np.expand_dims(warped_frame, axis=-1)
+    plt.imshow(curr_frame - warped_frame)
+    plt.title("Curr - Warped After 1")
+    plt.show()
+    diff = curr_frame - warped_frame
+    for i in range(47):
+        warped_frame = warp_flow(warped_frame.astype(np.float32), flow)
+    warped_frame = np.expand_dims(warped_frame, axis=-1)
+    plt.imshow(curr_frame - warped_frame)
+    plt.title("Curr - Warped after 48")
+    plt.show()
+    diff2 = curr_frame - warped_frame
+    plt.imshow(diff - diff2)
+    plt.title("Warped 1 minus Warped 2")
+    plt.show()
+    plt.imshow(
+        prev_frame - np.expand_dims(warp_flow(prev_frame.astype(np.float32), flow), axis=-1)
+    )
+    plt.title("Prev - Warped")
+    plt.show()
     exit()
-
-prvs = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
-
-next = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
