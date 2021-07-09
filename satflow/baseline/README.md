462207cb461e51d545321ff868fbe6ff87cdea38
Jacob Bieker <jacob@openclimatefix.org>
2021-07-09 09:08:32 +0100

    Add README with some of the prelim data

---
+++ satflow/baseline/README.md
@@ -0,0 +1,22 @@
+## Baseline
+
+To see if our ML models are actually improving the predictions of the cloud masks, we
+have benchmarked it against OpenCV's dense optical flow predictions, as well as a naive
+baseline of just predicting the current image for all future timesteps.
+
+As we come up with and implement more metrics to compare these models, they will be added
+here. Currently, the only metric tested is the mean squared error between the predicted frame
+and the ground truth frame. To get a sense if there is a temporal dependence, the mean loss is
+done not just for the overall predictions, but for each of the future timesteps, going up to 4 hours (48 timesteps)
+in the future.
+
+On average, the optical flow approach has an MSE of 0.1541. The naive baseline has a MSE of 0.1566,
+so optical flow beats out the naive baseline by about 1.6%.
+
+## Caveats
+
+We tried obtaining the optical flow of consecutive, or even very temporally separated the cloud masks,
+but the optical flow usually ended up not actually changing anything. Instead, we used the
+MSG HSV satellite channel to compute the optical flow. This was chosen as that is the highest
+resolution satellite channel available, and it resulted in optical flow actually computing some movement.
+This flow field was then applied to the cloud masks directly to obtain the flow results.
--- satflow/baseline/optical_flow.py
+++ satflow/baseline/optical_flow.py
@@ -34,7 +34,13 @@ def warp_flow(img, flow):
 debug = False
 total_losses = np.array([0.0 for _ in range(48)])  # Want to break down loss by future timestep
 count = 0
+baseline_losses = np.array([0.0 for _ in range(48)])  # Want to break down loss by future timestep
+overall_loss = 0.0
+overall_baseline = 0.0
+
 for data in dataset:
+    tmp_loss = 0
+    tmp_base = 0
     count += 1
     prev_frame, curr_frame, next_frames, image, prev_image = data
     prev_frame = np.moveaxis(prev_frame, [0], [2])
@@ -46,6 +52,14 @@ for data in dataset:
         torch.from_numpy(warped_frame), torch.from_numpy(np.expand_dims(next_frames[0], axis=-1))
     )
     total_losses[0] += loss.item()
+    tmp_loss += loss.item()
+    loss = F.mse_loss(
+        torch.from_numpy(curr_frame.astype(np.float32)),
+        torch.from_numpy(np.expand_dims(next_frames[0], axis=-1)),
+    )
+    baseline_losses[0] += loss.item()
+    tmp_base += loss.item()
+
     for i in range(1, 48):
         warped_frame = warp_flow(warped_frame.astype(np.float32), flow)
         warped_frame = np.expand_dims(warped_frame, axis=-1)
@@ -54,7 +68,19 @@ for data in dataset:
             torch.from_numpy(np.expand_dims(next_frames[i], axis=-1)),
         )
         total_losses[i] += loss.item()
+        tmp_loss += loss.item()
+        loss = F.mse_loss(
+            torch.from_numpy(curr_frame.astype(np.float32)),
+            torch.from_numpy(np.expand_dims(next_frames[i], axis=-1)),
+        )
+        baseline_losses[i] += loss.item()
+        tmp_base += loss.item()
+    tmp_base /= 48
+    tmp_loss /= 48
+    overall_loss += tmp_loss
+    overall_baseline += tmp_base
     print(
-        f"Avg Total Loss: {np.mean(total_losses) / count} Avg Loss per Timestep: {total_losses / count}"
+        f"Avg Total Loss: {np.mean(total_losses) / count} Avg Baseline Loss: {np.mean(baseline_losses) / count} \n Overall Loss: {overall_loss / count} Baseline: {overall_baseline / count}"
     )
 np.save("optical_flow_mse_loss.npy", total_losses / count)
+np.save("baseline_current_image_mse_loss.npy", baseline_losses / count)
