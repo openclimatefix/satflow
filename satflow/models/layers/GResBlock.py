1698d6a849808a9a758daa53363e03ea810510c8
Jacob Bieker <jacob@bieker.tech>
2021-07-09 16:11:32 +0100

    Start on adapting DVD-GAN from https://github.com/Harrypotterrrr/DVD-GAN

---
+++ satflow/models/layers/GResBlock.py
@@ -0,0 +1,118 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+from satflow.models.layers.Normalization import ConditionalNorm, SpectralNorm
+
+
+class GResBlock(nn.Module):
+    def __init__(
+        self,
+        in_channel,
+        out_channel,
+        kernel_size=None,
+        padding=1,
+        stride=1,
+        n_class=96,
+        bn=True,
+        activation=F.relu,
+        upsample_factor=2,
+        downsample_factor=1,
+    ):
+        super().__init__()
+
+        self.upsample_factor = upsample_factor if downsample_factor is 1 else 1
+        self.downsample_factor = downsample_factor
+        self.activation = activation
+        self.bn = bn if downsample_factor is 1 else False
+
+        if kernel_size is None:
+            kernel_size = [3, 3]
+
+        self.conv0 = SpectralNorm(
+            nn.Conv2d(
+                in_channel, out_channel, kernel_size, stride, padding, bias=True if bn else True
+            )
+        )
+        self.conv1 = SpectralNorm(
+            nn.Conv2d(
+                out_channel, out_channel, kernel_size, stride, padding, bias=True if bn else True
+            )
+        )
+
+        self.skip_proj = True
+        self.conv_sc = SpectralNorm(nn.Conv2d(in_channel, out_channel, 1, 1, 0))
+
+        # if in_channel != out_channel or upsample_factor or downsample_factor:
+        #     self.conv_sc = SpectralNorm(nn.Conv2d(in_channel, out_channel, 1, 1, 0))
+        #     self.skip_proj = True
+
+        if bn:
+            self.CBNorm1 = ConditionalNorm(in_channel, n_class)  # TODO 2 x noise.size[1]
+            self.CBNorm2 = ConditionalNorm(out_channel, n_class)
+
+    def forward(self, x, condition=None):
+
+        # The time dimension is combined with the batch dimension here, so each frame proceeds
+        # through the blocks independently
+        BT, C, W, H = x.size()
+        out = x
+
+        if self.bn:
+            out = self.CBNorm1(out, condition)
+
+        out = self.activation(out)
+
+        if self.upsample_factor != 1:
+            out = F.interpolate(out, scale_factor=self.upsample_factor)
+
+        out = self.conv0(out)
+
+        if self.bn:
+            out = out.view(BT, -1, W * self.upsample_factor, H * self.upsample_factor)
+            out = self.CBNorm2(out, condition)
+
+        out = self.activation(out)
+        out = self.conv1(out)
+
+        if self.downsample_factor != 1:
+            out = F.avg_pool2d(out, self.downsample_factor)
+
+        if self.skip_proj:
+            skip = x
+            if self.upsample_factor != 1:
+                skip = F.interpolate(skip, scale_factor=self.upsample_factor)
+            skip = self.conv_sc(skip)
+            if self.downsample_factor != 1:
+                skip = F.avg_pool2d(skip, self.downsample_factor)
+        else:
+            skip = x
+
+        y = out + skip
+        y = y.view(
+            BT,
+            -1,
+            W * self.upsample_factor // self.downsample_factor,
+            H * self.upsample_factor // self.downsample_factor,
+        )
+
+        return y
+
+
+if __name__ == "__main__":
+
+    n_class = 96
+    batch_size = 4
+    n_frames = 20
+
+    gResBlock = GResBlock(3, 100, [3, 3])
+    x = torch.rand([batch_size * n_frames, 3, 64, 64])
+    condition = torch.rand([batch_size, n_class])
+    condition = condition.repeat(n_frames, 1)
+    y = gResBlock(x, condition)
+    print(gResBlock)
+    print(x.size())
+    print(y.size())
+
+    # with SummaryWriter(comment='gResBlock') as w:
+    #     w.add_graph(gResBlock, [x, condition, ])
---
+++ satflow/models/layers/Generator.py
@@ -0,0 +1,152 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+from tensorboardX import SummaryWriter
+
+from satflow.models.layers.GResBlock import GResBlock
+from satflow.models.layers.Normalization import SpectralNorm
+from satflow.models.layers.ConvGRU import ConvGRU
+from satflow.models.layers.Attention import SelfAttention, SeparableAttn
+
+# from Module.CrossReplicaBN import ScaledCrossReplicaBatchNorm2d
+
+
+class Generator(nn.Module):
+    def __init__(self, in_dim=120, latent_dim=4, n_class=4, ch=32, n_frames=48, hierar_flag=False):
+        super().__init__()
+
+        self.in_dim = in_dim
+        self.latent_dim = latent_dim
+        self.n_class = n_class
+        self.ch = ch
+        self.hierar_flag = hierar_flag
+        self.n_frames = n_frames
+
+        self.embedding = nn.Embedding(n_class, in_dim)
+
+        self.affine_transfrom = nn.Linear(in_dim * 2, latent_dim * latent_dim * 8 * ch)
+
+        self.conv = nn.ModuleList(
+            [
+                ConvGRU(
+                    8 * ch,
+                    hidden_sizes=[8 * ch, 16 * ch, 8 * ch],
+                    kernel_sizes=[3, 5, 3],
+                    n_layers=3,
+                ),
+                # ConvGRU(8 * ch, hidden_sizes=[8 * ch, 8 * ch], kernel_sizes=[3, 3], n_layers=2),
+                GResBlock(8 * ch, 8 * ch, n_class=in_dim * 2, upsample_factor=1),
+                GResBlock(8 * ch, 8 * ch, n_class=in_dim * 2),
+                ConvGRU(
+                    8 * ch,
+                    hidden_sizes=[8 * ch, 16 * ch, 8 * ch],
+                    kernel_sizes=[3, 5, 3],
+                    n_layers=3,
+                ),
+                # ConvGRU(8 * ch, hidden_sizes=[8 * ch, 8 * ch], kernel_sizes=[3, 3], n_layers=2),
+                GResBlock(8 * ch, 8 * ch, n_class=in_dim * 2, upsample_factor=1),
+                GResBlock(8 * ch, 8 * ch, n_class=in_dim * 2),
+                ConvGRU(
+                    8 * ch,
+                    hidden_sizes=[8 * ch, 16 * ch, 8 * ch],
+                    kernel_sizes=[3, 5, 3],
+                    n_layers=3,
+                ),
+                # ConvGRU(8 * ch, hidden_sizes=[8 * ch, 8 * ch], kernel_sizes=[3, 3], n_layers=2),
+                GResBlock(8 * ch, 8 * ch, n_class=in_dim * 2, upsample_factor=1),
+                GResBlock(8 * ch, 4 * ch, n_class=in_dim * 2),
+                ConvGRU(
+                    4 * ch,
+                    hidden_sizes=[4 * ch, 8 * ch, 4 * ch],
+                    kernel_sizes=[3, 5, 5],
+                    n_layers=3,
+                ),
+                # ConvGRU(4 * ch, hidden_sizes=[4 * ch, 4 * ch], kernel_sizes=[3, 5], n_layers=2),
+                GResBlock(4 * ch, 4 * ch, n_class=in_dim * 2, upsample_factor=1),
+                GResBlock(4 * ch, 2 * ch, n_class=in_dim * 2),
+            ]
+        )
+
+        self.colorize = SpectralNorm(nn.Conv2d(2 * ch, 3, kernel_size=(3, 3), padding=1))
+
+    def forward(self, x, class_id):
+
+        if self.hierar_flag is True:
+            noise_emb = torch.split(x, self.in_dim, dim=1)
+        else:
+            noise_emb = x
+
+        class_emb = self.embedding(class_id)
+
+        if self.hierar_flag is True:
+            y = self.affine_transfrom(
+                torch.cat((noise_emb[0], class_emb), dim=1)
+            )  # B x (2 x ld x ch)
+        else:
+            y = self.affine_transfrom(
+                torch.cat((noise_emb, class_emb), dim=1)
+            )  # B x (2 x ld x ch)
+
+        y = y.view(-1, 8 * self.ch, self.latent_dim, self.latent_dim)  # B x ch x ld x ld
+
+        for k, conv in enumerate(self.conv):
+            if isinstance(conv, ConvGRU):
+
+                if k > 0:
+                    _, C, W, H = y.size()
+                    y = y.view(-1, self.n_frames, C, W, H).contiguous()
+
+                frame_list = []
+                for i in range(self.n_frames):
+                    if k == 0:
+                        if i == 0:
+                            frame_list.append(conv(y))  # T x [B x ch x ld x ld]
+                        else:
+                            frame_list.append(conv(y, frame_list[i - 1]))
+                    else:
+                        if i == 0:
+                            frame_list.append(
+                                conv(y[:, 0, :, :, :].squeeze(1))
+                            )  # T x [B x ch x ld x ld]
+                        else:
+                            frame_list.append(conv(y[:, i, :, :, :].squeeze(1), frame_list[i - 1]))
+                frame_hidden_list = []
+                for i in frame_list:
+                    frame_hidden_list.append(i[-1].unsqueeze(0))
+                y = torch.cat(frame_hidden_list, dim=0)  # T x B x ch x ld x ld
+
+                y = y.permute(1, 0, 2, 3, 4).contiguous()  # B x T x ch x ld x ld
+                # print(y.size())
+                B, T, C, W, H = y.size()
+                y = y.view(-1, C, W, H)
+
+            elif isinstance(conv, GResBlock):
+                condition = torch.cat([noise_emb, class_emb], dim=1)
+                condition = condition.repeat(self.n_frames, 1)
+                y = conv(y, condition)  # BT, C, W, H
+
+        y = F.relu(y)
+        y = self.colorize(y)
+        y = torch.tanh(y)
+
+        BT, C, W, H = y.size()
+        y = y.view(-1, self.n_frames, C, W, H)  # B, T, C, W, H
+
+        return y
+
+
+if __name__ == "__main__":
+
+    batch_size = 5
+    in_dim = 120
+    n_class = 4
+    n_frames = 4
+
+    x = torch.randn(batch_size, in_dim).cuda()
+    class_label = torch.randint(low=0, high=3, size=(batch_size,)).cuda()
+    generator = Generator(in_dim, n_class=n_class, ch=3, n_frames=n_frames).cuda()
+    y = generator(x, class_label)
+
+    print(x.size())
+    print(y.size())
