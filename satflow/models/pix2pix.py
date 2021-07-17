507bf65e62952650130c6e77f8f805ec9e9817a3
Jacob Bieker <jacob@openclimatefix.org>
2021-07-17 13:17:03 +0100

    Add visualizing Pix2Pix output

--- satflow/models/pix2pix.py
+++ satflow/models/pix2pix.py
@@ -1,5 +1,6 @@
 import torch
 import torchvision
+import numpy as np
 from collections import OrderedDict
 import torch.nn as nn
 from torch.nn import init
@@ -28,6 +29,7 @@ class Pix2Pix(pl.LightningModule):
         scheduler: str = "plateau",
         lr_epochs: int = 10,
         lambda_l1: float = 100.0,
+        channels_per_timestep: int = 12,
     ):
         super().__init__()
         self.save_hyperparameters()
@@ -41,6 +43,7 @@ class Pix2Pix(pl.LightningModule):
         self.forecast_steps = forecast_steps
         self.input_channels = input_channels
         self.output_channels = forecast_steps * 12
+        self.channels_per_timestep = channels_per_timestep
 
         # define networks (both generator and discriminator)
         self.generator = define_G(
@@ -63,6 +66,23 @@ class Pix2Pix(pl.LightningModule):
     def forward(self, x):
         return self.generator(x)
 
+    def visualize(self, x, y, y_hat, batch_idx, step):
+        # the logger you used (in this case tensorboard)
+        tensorboard = self.logger.experiment[0]
+        # Add all the different timesteps for a single prediction, 0.1% of the time
+        images = x[0].cpu().detach()
+        images = [torch.unsqueeze(img, dim=0) for img in images]
+        image_grid = torchvision.utils.make_grid(images, nrow=self.channels_per_timestep)
+        tensorboard.add_image(f"{step}/Input_Image_Stack", image_grid, global_step=batch_idx)
+        images = y[0].cpu().detach()
+        images = [torch.unsqueeze(img, dim=0) for img in images]
+        image_grid = torchvision.utils.make_grid(images, nrow=12)
+        tensorboard.add_image(f"{step}/Target_Image_Stack", image_grid, global_step=batch_idx)
+        images = y_hat[0].cpu().detach()
+        images = [torch.unsqueeze(img, dim=0) for img in images]
+        image_grid = torchvision.utils.make_grid(images, nrow=12)
+        tensorboard.add_image(f"{step}/Generated_Image_Stack", image_grid, global_step=batch_idx)
+
     def training_step(self, batch, batch_idx, optimizer_idx):
         images, future_images, future_masks = batch
         # train generator
@@ -71,6 +91,8 @@ class Pix2Pix(pl.LightningModule):
             generated_images = self(images)
             fake = torch.cat((images, generated_images), 1)
             # log sampled images
+            if np.random.random() < 0.01:
+                self.visualize(images, future_images, generated_images, batch_idx, step="train")
 
             # adversarial loss is binary cross-entropy
             gan_loss = self.criterionGAN(self.discriminator(fake), True)
@@ -78,6 +100,7 @@ class Pix2Pix(pl.LightningModule):
             g_loss = gan_loss + l1_loss
             tqdm_dict = {"g_loss": g_loss}
             output = OrderedDict({"loss": g_loss, "progress_bar": tqdm_dict, "log": tqdm_dict})
+            self.log_dict({"train/g_loss": g_loss})
             return output
 
         # train discriminator
@@ -97,7 +120,42 @@ class Pix2Pix(pl.LightningModule):
             d_loss = (real_loss + fake_loss) / 2
             tqdm_dict = {"d_loss": d_loss}
             output = OrderedDict({"loss": d_loss, "progress_bar": tqdm_dict, "log": tqdm_dict})
+            self.log_dict({"train/d_loss": d_loss})
             return output
+
+    def validation_step(self, batch, batch_idx):
+        images, future_images, future_masks = batch
+        # generate images
+        generated_images = self(images)
+        fake = torch.cat((images, generated_images), 1)
+        # log sampled images
+        if np.random.random() < 0.01:
+            self.visualize(images, future_images, generated_images, batch_idx, step="val")
+
+        # adversarial loss is binary cross-entropy
+        gan_loss = self.criterionGAN(self.discriminator(fake), True)
+        l1_loss = self.criterionL1(generated_images, future_images) * self.lambda_l1
+        g_loss = gan_loss + l1_loss
+        # how well can it label as real?
+        real = torch.cat((images, future_images), 1)
+        real_loss = self.criterionGAN(self.discriminator(real), True)
+
+        # how well can it label as fake?
+        fake_loss = self.criterionGAN(self.discriminator(fake), True)
+
+        # discriminator loss is the average of these
+        d_loss = (real_loss + fake_loss) / 2
+        tqdm_dict = {"d_loss": d_loss}
+        output = OrderedDict(
+            {
+                "val/discriminator_loss": d_loss,
+                "val/generator_loss": g_loss,
+                "progress_bar": tqdm_dict,
+                "log": tqdm_dict,
+            }
+        )
+        self.log_dict({"val/d_loss": d_loss, "val/g_loss": g_loss})
+        return output
 
     def configure_optimizers(self):
         lr = self.lr
