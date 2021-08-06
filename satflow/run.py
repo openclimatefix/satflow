e87d2b2093684e4dbd4ed58d8adc8cbb607b5d48
Jacob Bieker <jacob@openclimatefix.org>
2021-08-06 16:17:25 +0100

    Update output shape to match new preprocessing

--- satflow/run.py
+++ satflow/run.py
@@ -2,8 +2,6 @@ import dotenv
 import hydra
 from omegaconf import DictConfig
 
-from clearml import Task
-
 # load environment variables from `.env` file if it exists
 # recursively searches for `.env` in all folders starting from work dir
 dotenv.load_dotenv(override=True)
@@ -11,9 +9,6 @@ dotenv.load_dotenv(override=True)
 
 @hydra.main(config_path="configs/", config_name="config.yaml")
 def main(config: DictConfig):
-    task = Task.init(
-        project_name="Future Satellite Prediction", task_name="MetNet 12 Channels SSIM"
-    )
 
     # Imports should be nested inside @hydra.main to optimize tab completion
     # Read more here: https://github.com/facebookresearch/hydra/issues/934
