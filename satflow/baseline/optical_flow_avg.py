7a1f9a615d0e45cf59eaafb388e4fe37dc7908c7
Jacob Bieker <jacob@openclimatefix.org>
2021-08-06 14:00:13 +0100

    Add the averaging and SSIM, make a bit cleaner

--- satflow/baseline/optical_flow_avg.py
+++ satflow/baseline/optical_flow_avg.py
@@ -4,6 +4,7 @@ import webdataset as wds
 import yaml
 import torch.nn.functional as F
 import numpy as np
+from satflow.models.losses import SSIMLoss
 
 
 def load_config(config_file):
@@ -11,13 +12,14 @@ def load_config(config_file):
         return yaml.load(cfg, Loader=yaml.FullLoader)["config"]
 
 
+used_loss = SSIMLoss(data_range=1.0, nonnegative_ssim=True, channel=3)
+
 config = load_config(
     "/home/jacob/Development/satflow/satflow/configs/datamodule/optical_flow_datamodule.yaml"
 )
 dset = wds.WebDataset("/run/media/jacob/data/satflow-flow-144-tiled-{00001..00149}.tar")
 
 dataset = SatFlowDataset([dset], config=config)
-
 import matplotlib.pyplot as plt
 import torch
 
@@ -49,8 +51,6 @@ for data in dataset:
     curr_frame = past_frames[1]
     # Do it for each of the 12 channels
     for ch in range(12):
-        # prev_frame = np.moveaxis(prev_frame, [0], [2])
-        # curr_frame = np.moveaxis(curr_frame, [0], [2])
         flow = cv2.calcOpticalFlowFarneback(
             past_frames[0][ch].astype(np.float32),
             past_frames[1][ch].astype(np.float32),
@@ -113,39 +113,63 @@ for data in dataset:
         )
         flow /= 5
         warped_frame = warp_flow(past_frames[5][ch].astype(np.float32), flow)
-        warped_frame = np.expand_dims(warped_frame, axis=-1)
-        loss = F.mse_loss(
-            torch.from_numpy(warped_frame),
-            torch.from_numpy(np.expand_dims(next_frames[0][ch].astype(np.float32), axis=-1)),
+        warped_frame2 = np.expand_dims(warped_frame, axis=0)
+        warped_frame2 = np.stack((warped_frame2,) * 3, axis=1)
+        next_frame = np.expand_dims(next_frames[0][ch].astype(np.float32), axis=0)
+        next_frame = np.stack((next_frame,) * 3, axis=1)
+        # Force between 0 and 1
+        next_frame = (next_frame - np.min(next_frame)) / (np.max(next_frame) - np.min(next_frame))
+        warped_frame2 = (warped_frame2 - np.min(warped_frame2)) / (
+            np.max(warped_frame2) - np.min(warped_frame2)
+        )
+        loss = used_loss(
+            torch.from_numpy(warped_frame2),
+            torch.from_numpy(next_frame),
         )
         channel_total_losses[ch][0] += loss.item()
-        loss = F.mse_loss(
-            torch.from_numpy(past_frames[5][ch].astype(np.float32)),
-            torch.from_numpy(next_frames[0][ch].astype(np.float32)),
+        current_frame = np.expand_dims(past_frames[-1][ch].astype(np.float32), axis=0)
+        current_frame = np.stack((current_frame,) * 3, axis=1)
+        current_frame = (current_frame - np.min(current_frame)) / (
+            np.max(current_frame) - np.min(current_frame)
+        )
+        loss = used_loss(
+            torch.from_numpy(current_frame),
+            torch.from_numpy(next_frame),
         )
         channel_baseline_losses[ch][0] += loss.item()
-
+        warped_frame = np.expand_dims(warped_frame, axis=-1)
         for i in range(1, 48):
             warped_frame = warp_flow(warped_frame.astype(np.float32), flow)
-            warped_frame = np.expand_dims(warped_frame, axis=-1)
-            loss = F.mse_loss(
-                torch.from_numpy(warped_frame),
-                torch.from_numpy(np.expand_dims(next_frames[i][ch].astype(np.float32), axis=-1)),
+            warped_frame2 = np.expand_dims(warped_frame, axis=0)
+            warped_frame2 = np.stack((warped_frame2,) * 3, axis=1)
+            next_frame = np.expand_dims(next_frames[0][ch].astype(np.float32), axis=0)
+            next_frame = np.stack((next_frame,) * 3, axis=1)
+            # Force between 0 and 1
+            next_frame = (next_frame - np.min(next_frame)) / (
+                np.max(next_frame) - np.min(next_frame)
+            )
+            warped_frame2 = (warped_frame2 - np.min(warped_frame2)) / (
+                np.max(warped_frame2) - np.min(warped_frame2)
+            )
+            loss = used_loss(
+                torch.from_numpy(warped_frame2),
+                torch.from_numpy(next_frame),
             )
             channel_total_losses[ch][i] += loss.item()
-            loss = F.mse_loss(
-                torch.from_numpy(past_frames[5][ch].astype(np.float32)),
-                torch.from_numpy(next_frames[i][ch].astype(np.float32)),
+            loss = used_loss(
+                torch.from_numpy(current_frame),
+                torch.from_numpy(next_frame),
             )
             channel_baseline_losses[ch][i] += loss.item()
+            warped_frame = np.expand_dims(warped_frame, axis=-1)
     print(
         f"Avg Total Loss: {np.mean(channel_total_losses) / count} Avg Baseline Loss: {np.mean(channel_baseline_losses) / count}"
     )
-    if count % 100 == 0:
-        np.save("optical_flow_mse_loss_channels_avg_pairs.npy", channel_total_losses / count)
+    if count % 10 == 0:
+        np.save("optical_flow_ssim_loss_avg_pairs.npy", channel_total_losses / count)
         np.save(
-            "baseline_current_image_mse_loss_channels_avg_pairs.npy",
+            "baseline_ssim_loss_channels_avg_pairs.npy",
             channel_baseline_losses / count,
         )
-np.save("optical_flow_mse_loss_avg_pairs.npy", channel_total_losses / count)
-np.save("baseline_current_image_mse_loss_avg_pairs.npy", channel_baseline_losses / count)
+# np.save("optical_flow_mse_loss_avg_pairs.npy", channel_total_losses / count)
+# np.save("baseline_current_image_mse_loss_avg_pairs.npy", channel_baseline_losses / count)
--- satflow/baseline/plot_flow.py
+++ satflow/baseline/plot_flow.py
@@ -16,13 +16,13 @@ bands = [
     "WV073",
 ]
 
-baseline = np.load("baseline_current_image_mse_loss_channels.npy")
-baseline_reverse = np.load("baseline_current_image_mse_loss_channels_reverse.npy")
-flow = np.load("optical_flow_mse_loss_channels.npy")
-flow_reverse = np.load("optical_flow_mse_loss_channels_reverse.npy")
+baseline = np.load("baseline_current_image_mse_loss_channels_avg_base_only.npy")
+flow = np.load("optical_flow_mse_loss_channels_avg_pairs.npy")
+ssim = np.load("optical_flow_ssim_loss_channels_two_frames.npy")
+ssim_base = np.load("baseline_current_image_ssim_loss_channels_baseline.npy")
+
 
 # Now slice it up by channel and by timestep
-print(baseline_reverse.shape)
 per_channel_base = np.mean(baseline, axis=1)
 per_channel_total = np.mean(flow, axis=1)
 plt.plot(per_channel_base, label="Current Image")
@@ -32,6 +32,16 @@ plt.xlabel("Channel #")
 plt.ylabel("MSE")
 plt.title("Mean MSE per channel")
 plt.savefig("mse_per_channel.png", dpi=300)
+plt.show()
+per_channel_base = 1.0 - np.mean(ssim_base, axis=1)
+per_channel_total = 1.0 - np.mean(ssim, axis=1)
+plt.plot(per_channel_base, label="Current Image")
+plt.plot(per_channel_total, label="Optical Flow")
+plt.legend(loc="best")
+plt.xlabel("Channel #")
+plt.ylabel("SSIM")
+plt.title("Mean SSIM per channel")
+plt.savefig("ssim_per_channel.png", dpi=300)
 plt.show()
 
 per_channel_base = np.mean(baseline, axis=0)
@@ -44,6 +54,16 @@ plt.ylabel("MSE")
 plt.title("Mean MSE per timestep")
 plt.savefig("mse_per_timestep.png", dpi=300)
 plt.show()
+per_channel_base = 1.0 - np.mean(ssim_base, axis=0)
+per_channel_total = 1.0 - np.mean(ssim, axis=0)
+plt.plot(per_channel_base, label="Current Image")
+plt.plot(per_channel_total, label="Optical Flow")
+plt.legend(loc="best")
+plt.xlabel("Timestep")
+plt.ylabel("SSIM")
+plt.title("Mean SSIM per timestep")
+plt.savefig("ssim_per_timestep.png", dpi=300)
+plt.show()
 
 for i in range(12):
     per_channel_base = baseline[i]
@@ -55,4 +75,16 @@ for i in range(12):
     plt.ylabel("MSE")
     plt.title(f"Mean MSE per timestep ({bands[i]})")
     plt.savefig(f"mse_{bands[i]}.png", dpi=300)
+    plt.show()
+
+for i in range(12):
+    per_channel_base = 1.0 - ssim_base[i]
+    per_channel_total = 1.0 - ssim[i]
+    plt.plot(per_channel_base, label="Current Image")
+    plt.plot(per_channel_total, label="Optical Flow")
+    plt.legend(loc="best")
+    plt.xlabel("Timestep")
+    plt.ylabel("SSIM")
+    plt.title(f"Mean SSIM per timestep ({bands[i]})")
+    plt.savefig(f"ssim_{bands[i]}.png", dpi=300)
     plt.show()
