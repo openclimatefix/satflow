4e06a99c7dace111c6b5e975a6b5f5196ab22e0c
Jacob Bieker <jacob@openclimatefix.org>
2021-08-06 16:35:25 +0100

    Add MetNet normalization step

---
+++ satflow/data/utils/normalization.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+def metnet_normalization(data: np.ndarry) -> np.ndarray:
+    """
+    Perform the normalization used in the MetNet paper on the GOES data
+    This involves subtracting by the median, dividing by the interquartile range,
+    then squashing to [-1,1] with the hyperbolic tangent
+    Args:
+        data:
+
+    Returns:
+
+    """
+    # Ensure no NaNs
+    data = np.nan_to_num(data)
+    # Get IQR
+    iqr = np.subtract(*np.percentile(data, [75, 25]))
+    data = (data - np.median(data)) / iqr
+    # Now hyperbolic tangent
+    return np.tanh(data)
