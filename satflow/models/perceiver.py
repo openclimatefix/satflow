059866a2cba416cea0df076a65db5ecc4c790918
Jacob Bieker <jacob@openclimatefix.org>
2021-08-10 16:16:29 +0100

    Fix imports, remove other Perceiver implementation

--- satflow/models/perceiver.py
+++ satflow/models/perceiver.py
@@ -4,10 +4,11 @@ from satflow.models.losses import get_loss, NowcastingLoss, GridCellLoss
 import pytorch_lightning as pl
 import torchvision
 from functools import reduce
-from typing import List
+from typing import List, Iterable, Dict
 from satflow.models.base import register_model
 from math import pi, log
 from einops import rearrange, repeat
+from satflow.models.layers.modalities import modality_encoding, InputModality
 
 
 @register_model
@@ -212,11 +213,11 @@ class PerceiverSat(torch.nn.Module):
             # Figure out padding for this modality, given max dimension across all modalities:
             padding_size = self.max_modality_dim - modality.input_dim - num_modalities
 
-            padding = torch.zeros(size=data.size()[0:-1] + (padding_size,)).to(device)
+            padding = torch.zeros(size=data.size()[0:-1] + (padding_size,)).type_as(data)
             # concat to channels of data and flatten axis
             modality_encodings = modality_encoding(
-                b, axis, modality_index, num_modalities, device=device
-            )
+                b, axis, modality_index, num_modalities
+            ).type_as(data)
 
             to_concat = (
                 (data, padding, enc_pos, modality_encodings)
@@ -225,15 +226,12 @@ class PerceiverSat(torch.nn.Module):
             )
 
             data = torch.cat(to_concat, dim=-1)
+            # concat to channels of data and flatten axis
             data = rearrange(data, "b ... d -> b (...) d")
             linearized_data.append(data)
-        b = batch_sizes.pop()
-        x = repeat(self.latents, "n d -> b n d", b=b)
 
         # Concatenate all the modalities:
         data = torch.cat(linearized_data, dim=1)
-        # concat to channels of data and flatten axis
-        data = rearrange(data, "b ... d -> b (...) d")
 
         # After this is the PerceiverIO backbone, still would need to decode it back to an image though
         perceiver_output = self.perceiver.forward(data, mask, queries)
@@ -241,123 +239,3 @@ class PerceiverSat(torch.nn.Module):
         # Have to decode back into future Sat image frames
 
         return NotImplementedError
-
-
-class MultiModalityPerceiver(nn.Module):
-    def __init__(
-        self,
-        *,
-        modalities: Iterable[InputModality],
-        depth,
-        num_latents=512,
-        latent_dim=512,
-        cross_heads=1,
-        latent_heads=8,
-        cross_dim_head=64,
-        latent_dim_head=64,
-        num_classes=None,
-        attn_dropout=0.0,
-        ff_dropout=0.0,
-        weight_tie_layers=False,
-        num_latent_blocks_per_layer=1,
-        use_gelu: bool = False,
-    ):
-        """
-        :param modalities:
-        :param depth: Number of times the perceiver will perform cross-attention between latent and input.
-        :param num_latents:
-        :param latent_dim:
-        :param cross_heads:
-        :param latent_heads:
-        :param cross_dim_head:
-        :param latent_dim_head:
-        :param num_classes: Number of classes to predict, or if None, return the hidden state (num latents x hidden_dim)
-        :param attn_dropout:
-        :param ff_dropout:
-        :param weight_tie_layers: True: share weights across layers, False no shared weights.
-        :param num_latent_blocks_per_layer: Number of blocks in the latent transformer.
-        :param use_gelu: Use GELU activation like the Perceiver preprint indicates. False,
-               with Lucidrains' GEGLU activation in feed forward instead.
-        """
-        super().__init__()
-        self.modalities = {modality.name: modality for modality in modalities}
-        # we encode modality with one hot encoding, so need one dim per modality:
-        modality_encoding_dim = sum([1 for _ in modalities])
-        # input_dim is the maximum dimension over all input modalities:
-        input_dim = max(modality.input_dim for modality in modalities) + modality_encoding_dim
-        self.max_modality_dim = input_dim
-        self.latents = nn.Parameter(torch.randn(num_latents, latent_dim))
-
-    def forward(self, multi_modality_data: Dict[str, Tensor], mask=None):
-        """
-        :param data: a dictionary where keys are modality names and Tensor contain a batch
-        of modality input data.
-        :param mask:
-        :return:
-        """
-        batch_sizes = set()
-        num_modalities = len(multi_modality_data)
-        linearized_data = []
-        linearized_data_per_layer: Dict[int, List[Tensor]] = {}
-
-        for modality_index, modality_name in enumerate(sorted(multi_modality_data.keys())):
-            assert (
-                modality_name in self.modalities
-            ), f"modality {modality_name} was not defined in constructor"
-            data = multi_modality_data[modality_name]
-            modality = self.modalities[modality_name]
-            b, *axis, _, device = *data.shape, data.device
-            assert len(axis) == modality.input_axis, (
-                f"input data must have the right number of  for modality {modality_name}. "
-                f"Expected {modality.input_axis} while forward argument offered {len(axis)}"
-            )
-            batch_sizes.add(b)
-            assert len(batch_sizes) == 1, "batch size must be the same across all modalities"
-            # calculate fourier encoded positions in the range of [-1, 1], for all axis
-
-            axis_pos = list(
-                map(lambda size: torch.linspace(-1.0, 1.0, steps=size, device=device), axis)
-            )
-            pos = torch.stack(torch.meshgrid(*axis_pos), dim=-1)
-            enc_pos = fourier_encode(
-                pos, modality.max_freq, modality.num_freq_bands, modality.freq_base
-            )
-            enc_pos = rearrange(enc_pos, "... n d -> ... (n d)")
-            enc_pos = repeat(enc_pos, "... -> b ...", b=b)
-
-            # Figure out padding for this modality, given max dimension across all modalities:
-            padding_size = self.max_modality_dim - modality.input_dim - num_modalities
-
-            padding = torch.zeros(size=data.size()[0:-1] + (padding_size,)).to(device)
-            # concat to channels of data and flatten axis
-            modality_encodings = modality_encoding(
-                b, axis, modality_index, num_modalities, device=device
-            )
-
-            to_concat = (data, padding, enc_pos, modality_encodings)
-
-            data = torch.cat(to_concat, dim=-1)
-            data = rearrange(data, "b ... d -> b (...) d")
-            linearized_data.append(data)
-        b = batch_sizes.pop()
-        x = repeat(self.latents, "n d -> b n d", b=b)
-
-        # Concatenate all the modalities:
-        data = torch.cat(linearized_data, dim=1)
-
-        for cross_attn, cross_ff, latent_transformer in self.layers:
-            x = cross_attn(x, context=data, mask=mask) + x
-            x = cross_ff(x) + x
-            x = latent_transformer(x) + x
-        x = self.pool(x)
-
-        return self.to_logits(x)
-
-        def pool(self, x):
-            """
-            Perform pooling over latents.
-            :param x: batch x num_latents x latent_dim
-            :return: pooled x
-            """
-            # implement global pooling
-            return x.mean(dim=-2)
