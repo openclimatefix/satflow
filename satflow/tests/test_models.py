151e2962ce0f30fd8382a61dcaa60e122db4026b
Jacob Bieker <jacob@openclimatefix.org>
2021-08-23 13:14:27 +0100

    Fix pop

--- satflow/tests/test_models.py
+++ satflow/tests/test_models.py
@@ -11,7 +11,7 @@ def load_config(config_file):
 
 def test_perceiver_creation():
     config = load_config("satflow/configs/model/perceiver_metnet.yaml")
-    config.pop("__target__")  # This is only for Hydra
+    config.pop("_target_")  # This is only for Hydra
     model = Perceiver(**config)
     x = torch.randn((2, 12, config["input_channels"], config["input_size"], config["input_size"]))
     model.eval()
@@ -30,13 +30,12 @@ def test_perceiver_creation():
 
 def test_nowcasting_gan_creation():
     config = load_config("satflow/configs/model/nowcasting_gan.yaml")
-    config.pop("__target__")  # This is only for Hydra
+    config.pop("_target_")  # This is only for Hydra
     model = NowcastingGAN(**config)
     x = torch.randn(
-        (2, 12, config["input_channels"], config["output_shape"], config["output_shape"])
+        (2, 4, config["input_channels"], config["output_shape"], config["output_shape"])
     )
     model.eval()
-    # TODO Get the Query/etc. like in the train/etc. Will currently return the embedding otherwise as of now
     with torch.no_grad():
         out = model(x)
     # MetNet creates predictions for the center 1/4th
@@ -51,7 +50,7 @@ def test_nowcasting_gan_creation():
 
 def test_metnet_creation():
     config = load_config("satflow/configs/model/metnet.yaml")
-    config.pop("__target__")  # This is only for Hydra
+    config.pop("_target_")  # This is only for Hydra
     model = MetNet(**config)
     x = torch.randn((2, 12, config["input_channels"], config["input_size"], config["input_size"]))
     model.eval()
