85193e7ed34c58b559f076c3daff80c0d95e87ec
Jacob Bieker <jacob@openclimatefix.org>
2021-09-06 11:02:01 +0100

    Remove unused imports

--- satflow/models/metnet.py
+++ satflow/models/metnet.py
@@ -6,40 +6,10 @@ import torch.nn.functional as F
 from satflow.models.base import register_model, BaseModel
 from satflow.models.utils import get_conv_layer
 from satflow.models.losses import get_loss
-from satflow.models.layers import ConvGRU2, TimeDistributed, ConditionTime, MetNetPreprocessor
-from axial_attention import AxialAttention
 from pl_bolts.optimizers.lr_scheduler import LinearWarmupCosineAnnealingLR
 import numpy as np
 import antialiased_cnns
 from metnet import MetNet
-
-
-class DownSampler(nn.Module):
-    def __init__(self, in_channels, output_channels: int = 256, conv_type: str = "standard"):
-        super().__init__()
-        conv2d = get_conv_layer(conv_type=conv_type)
-        self.output_channels = output_channels
-        if conv_type == "antialiased":
-            antialiased = True
-        else:
-            antialiased = False
-
-        self.module = nn.Sequential(
-            conv2d(in_channels, 160, 3, padding=1),
-            nn.MaxPool2d((2, 2), stride=1 if antialiased else 2),
-            antialiased_cnns.BlurPool(160, stride=2) if antialiased else nn.Identity(),
-            nn.BatchNorm2d(160),
-            conv2d(160, output_channels, 3, padding=1),
-            nn.BatchNorm2d(output_channels),
-            conv2d(output_channels, output_channels, 3, padding=1),
-            nn.BatchNorm2d(output_channels),
-            conv2d(output_channels, output_channels, 3, padding=1),
-            nn.MaxPool2d((2, 2), stride=1 if antialiased else 2),
-            antialiased_cnns.BlurPool(output_channels, stride=2) if antialiased else nn.Identity(),
-        )
-
-    def forward(self, x):
-        return self.module.forward(x)
 
 
 @register_model
@@ -135,19 +105,3 @@ class LitMetNet(BaseModel):
         y_hat = self(x)
         loss = self.criterion(y_hat, y)
         return loss
-
-
-class TemporalEncoder(nn.Module):
-    def __init__(self, in_channels, out_channels=384, ks=3, n_layers=1):
-        super().__init__()
-        self.rnn = ConvGRU2(in_channels, out_channels, (ks, ks), n_layers, batch_first=True)
-
-    def forward(self, x):
-        x, h = self.rnn(x)
-        return (x, h[-1])
-
-
-def feat2image(x, target_size=(128, 128)):
-    "This idea comes from MetNet"
-    x = x.transpose(1, 2)
-    return x.unsqueeze(-1).unsqueeze(-1) * x.new_ones(1, 1, 1, *target_size)
