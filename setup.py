85923a7ea6d25ffc225ce213d7b839c953630894
Jacob Bieker <jacob@openclimatefix.org>
2021-09-08 13:36:39 +0100

    Update version

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from distutils.core import setup
 
+with open("requirements.txt") as f:
+    install_requires = f.read().splitlines()
 exec(open("satflow/version.py").read())
 setup(
     name="satflow",
@@ -9,6 +11,7 @@ setup(
     license="MIT License",
     company="Open Climate Fix Ltd",
     author="Jacob Bieker",
+    install_requires=install_requires,
     author_email="jacob@openclimatefix.org",
     description="Satellite Optical Flow",
 )
