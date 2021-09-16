8348b3f91bbab9d664625f08646fe27192272b4b
Jacob Bieker <jacob@openclimatefix.org>
2021-09-16 10:38:24 +0100

    Update for newer NetCDFDataset from https://github.com/openclimatefix/nowcasting_dataset/pull/107

--- satflow/data/datasets.py
+++ satflow/data/datasets.py
@@ -1,5 +1,6 @@
 from typing import Tuple, Union, List
-from nowcasting_dataset.dataset import NetCDFDataset
+from nowcasting_dataset.dataset.datasets import NetCDFDataset
+from nowcasting_dataset.dataset.example import Example
 
 
 class SatFlowDataset(NetCDFDataset):
@@ -21,6 +22,9 @@ class SatFlowDataset(NetCDFDataset):
             "sat_x_coords",
             "sat_y_coords",
         ),
+        history_minutes: int = 30,
+        forecast_minutes: int = 60,
+        current_timestep_index: int = 7,
     ):
         """
         Args:
@@ -30,11 +34,23 @@ class SatFlowDataset(NetCDFDataset):
           tmp_path: The full path to the local temporary directory
             (on a local filesystem).
         """
-        super().__init__(n_batches, src_path, tmp_path, cloud, required_keys)
+        super().__init__(
+            n_batches,
+            src_path,
+            tmp_path,
+            cloud,
+            required_keys,
+            history_minutes,
+            forecast_minutes,
+            current_timestep_index,
+        )
+        # SatFlow specific changes, i.e. which timestep to split on
+        self.history_steps = history_minutes // 5
+        self.forecast_steps = forecast_minutes // 5
+        self.current_index = self.history_steps + 1
         self.required_keys = list(required_keys)
-        # SatFlow specific changes to it
 
-    def __getitem__(self, batch_idx: int) -> example.Example:
+    def __getitem__(self, batch_idx: int):
         """Returns a whole batch at once.
 
         Args:
@@ -45,51 +61,8 @@ class SatFlowDataset(NetCDFDataset):
             NamedDict where each value is a numpy array. The size of this
             array's first dimension is the batch size.
         """
-        if not 0 <= batch_idx < self.n_batches:
-            raise IndexError(
-                "batch_idx must be in the range" f" [0, {self.n_batches}), not {batch_idx}!"
-            )
-        netcdf_filename = nd_utils.get_netcdf_filename(batch_idx)
-        remote_netcdf_filename = os.path.join(self.src_path, netcdf_filename)
-        local_netcdf_filename = os.path.join(self.tmp_path, netcdf_filename)
+        batch = super().__getitem__(batch_idx)
 
-        if self.cloud == "gcp":
-            gcp_download_to_local(
-                remote_filename=remote_netcdf_filename,
-                local_filename=local_netcdf_filename,
-                gcs=self.gcs,
-            )
-        else:
-            aws_download_to_local(
-                remote_filename=remote_netcdf_filename,
-                local_filename=local_netcdf_filename,
-                s3_resource=self.s3_resource,
-            )
-
-        netcdf_batch = xr.load_dataset(local_netcdf_filename)
-        os.remove(local_netcdf_filename)
-
-        batch = example.Example(
-            sat_datetime_index=netcdf_batch.sat_time_coords,
-            nwp_target_time=netcdf_batch.nwp_time_coords,
-        )
-        for key in self.required_keys + list(example.DATETIME_FEATURE_NAMES):
-            try:
-                batch[key] = netcdf_batch[key]
-            except KeyError:
-                pass
-
-        sat_data = batch["sat_data"]
-        # subset data here?
-
-        if sat_data.dtype == np.int16:
-            sat_data = sat_data.astype(np.float32)
-            sat_data = sat_data - SAT_MEAN
-            sat_data /= SAT_STD
-            batch["sat_data"] = sat_data
-
-        batch = example.to_numpy(batch)
-
-        # Now do the subsetting and
+        # Need to partition out past and future sat images here, along with the rest of the data
 
         return batch
