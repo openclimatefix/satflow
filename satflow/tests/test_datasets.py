f78e70296be5b6257fd5bb55b6cff7afb9baf4c4
Jacob Bieker <jacob@openclimatefix.org>
2021-09-17 18:32:24 +0100

    Move tests outside of package

--- satflow/tests/test_datasets.py
+++
@@ -1,5 +0,0 @@
-from satflow.data.datasets import SatFlowDataset
-
-
-def test_dataset():
-    pass
---
+++ tests/test_datasets.py
@@ -0,0 +1,36 @@
+from satflow.data.datasets import SatFlowDataset
+from nowcasting_dataset.consts import (
+    SATELLITE_DATA,
+    SATELLITE_X_COORDS,
+    SATELLITE_Y_COORDS,
+    SATELLITE_DATETIME_INDEX,
+    NWP_DATA,
+    NWP_Y_COORDS,
+    NWP_X_COORDS,
+    NWP_TARGET_TIME,
+    DATETIME_FEATURE_NAMES,
+)
+
+
+def test_dataset():
+    train_dataset = SatFlowDataset(
+        1,
+        "test/train",
+        "test/train",
+        cloud="local",
+        required_keys=[
+            NWP_DATA,
+            NWP_X_COORDS,
+            NWP_Y_COORDS,
+            SATELLITE_DATA,
+            SATELLITE_X_COORDS,
+            SATELLITE_Y_COORDS,
+            SATELLITE_DATETIME_INDEX,
+            NWP_TARGET_TIME,
+        ],
+        history_minutes=10,
+        forecast_minutes=10,
+    )
+
+    sample, target = next(iter(train_dataset))
+    print(sample.keys())
