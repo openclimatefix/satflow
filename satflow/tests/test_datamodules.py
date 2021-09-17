56f78cb31bec1e586c08058461570eb35556f137
Jacob Bieker <jacob@openclimatefix.org>
2021-09-17 17:19:01 +0100

    Fix test

--- satflow/tests/test_datamodules.py
+++ satflow/tests/test_datamodules.py
@@ -5,5 +5,6 @@ def test_datamodule_subsetting():
     dataset = SatFlowDataModule(fake_data=True)
     dataset.setup()
     train_dset = dataset.train_dataloader()
-    sample = next(iter(train_dset))
-    assert sample["sat_data"].size() == (32, 19, 16, 16, 12)
+    sample, target = next(iter(train_dset))
+    assert sample["sat_data"].shape == (32, 7, 16, 16, 12)
+    assert target["sat_data"].shape == (32, 48, 16, 16, 12)
