d060b4ef2dbbae9719c2481e661a2cac4fb773ea
Jacob Bieker <jacob@bieker.tech>
2021-11-22 09:27:06 +0000

    Fix shapes

--- satflow/models/joint_model.py
+++ satflow/models/joint_model.py
@@ -494,10 +494,10 @@ class JointPerceiver(BaseModel):
         print(f"Change output: {y[GSP_YIELD][:,:,0].shape}")
         print(y[GSP_ID].shape)
         print(y[GSP_ID])
-        loss = self.gsp_criterion(y[GSP_YIELD], gsp_y_hat)
+        loss = self.gsp_criterion(y[GSP_YIELD][:,:,0], gsp_y_hat)
         self.log_dict({f"{'train' if is_training else 'val'}/gsp_loss": loss})
         for f in range(self.forecast_gsp_steps):
-            frame_loss = self.gsp_criterion(gsp_y_hat[:, f, :], y[GSP_YIELD][:, f, :]).item()
+            frame_loss = self.gsp_criterion(gsp_y_hat[:, f], y[GSP_YIELD][:, f, 0]).item()
             frame_loss_dict[
                 f"{'train' if is_training else 'val'}/gsp_timestep_{f}_loss"
             ] = frame_loss
